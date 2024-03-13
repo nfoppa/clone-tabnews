4e08514bc5dd74c7646fba8543a4dbed4a975365
nfoppa <87817615+nfoppa@users.noreply.github.com>
2024-03-13 20:18:26 +0000

    added emoji

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1>“Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.” 
-    ― Steve Jobs</h1>
+    return (
+        <>
+            <h1>“Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.” 
+            ― Steve Jobs 🍏</h1>
+        </>
+    )
 }
 
 export default Home;
