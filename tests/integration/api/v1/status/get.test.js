bd0eae5c964b1f95c0df4ff911567b7ecd0e5fd9
nfoppa <87817615+nfoppa@users.noreply.github.com>
2024-04-07 15:04:39 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,15 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  const database = responseBody.dependencies.database;
+
+  expect(database.version).toEqual("16.0");
+  expect(database.max_connections).toEqual(100);
+  expect(database.opened_connections).toEqual(1);
 });
