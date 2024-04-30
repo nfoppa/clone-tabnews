080ca59513b7ea49a68442dd5f5d810f4bb83a89
nfoppa <87817615+nfoppa@users.noreply.github.com>
2024-04-30 10:58:06 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,19 +7,11 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
-  console.log("Postgres Credentials", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
-    client.connect();
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
@@ -33,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
